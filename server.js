b507cc04bc54ccdafd084ca4df115db2ae2e98f5
TheOarsman <heinzulrichv@gmail.com>
2024-03-19 11:02:04 -0400

    server & package.json fixed

--- server.js
+++ server.js
@@ -5,57 +5,72 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.urlencoded({ extended: true }));
+// Middleware for JSON parsing
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
 app.use(express.static("public"));
 
 // Routes
+// GET /notes should return the notes.html file
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
+// GET * should return the index.html file
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// GET /api/notes should read the db.json file and return all saved notes as JSON
 app.get("/api/notes", (req, res) => {
-  fs.readFile("db/db.json", "utf8", (err, data) => {
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
     if (err) throw err;
     const notes = JSON.parse(data);
     res.json(notes);
   });
 });
 
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
-  fs.readFile("db/db.json", "utf8", (err, data) => {
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
     if (err) throw err;
     const notes = JSON.parse(data);
     // Assign a unique id to the new note
     newNote.id = notes.length + 1;
     notes.push(newNote);
-    fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
-      if (err) throw err;
-      res.json(newNote);
-    });
+    fs.writeFile(
+      path.join(__dirname, "db", "db.json"),
+      JSON.stringify(notes),
+      (err) => {
+        if (err) throw err;
+        res.json(newNote);
+      }
+    );
   });
 });
 
+// DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete
 app.delete("/api/notes/:id", (req, res) => {
-  const noteId = parseInt(req.params.id);
-  fs.readFile("db/db.json", "utf8", (err, data) => {
+  const id = parseInt(req.params.id);
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
     if (err) throw err;
     let notes = JSON.parse(data);
-    notes = notes.filter((note) => note.id !== noteId);
-    fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
-      if (err) throw err;
-      res.json({ msg: "Note deleted successfully" });
-    });
+    notes = notes.filter((note) => note.id !== id);
+    fs.writeFile(
+      path.join(__dirname, "db", "db.json"),
+      JSON.stringify(notes),
+      (err) => {
+        if (err) throw err;
+        res.sendStatus(200);
+      }
+    );
   });
 });
 
-// Start server
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
